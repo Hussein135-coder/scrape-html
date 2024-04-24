ea069392241f740289fcaf9cbe9e4884aa7595f3
Hussein135-coder <80627987+Hussein135-coder@users.noreply.github.com>
2024-04-24 12:35:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+const http = require("http");
+//import http from "http";
+
+const fetchUrl = async (url) => {
+  try {
+    const data = await fetch(url, {
+      rejectUnauthorized: false,
+    });
+    const html = await data.text();
+    return html;
+  } catch (error) {
+    console.log(error);
+    return "Error";
+  }
+};
+
+const listener = http.createServer(async (req, res) => {
+  console.log("req.headers.autorization", req.headers.authorization);
+  if (req.method == "POST") {
+    console.log("POST");
+    var body = "";
+    req.on("data", async function (data) {
+      body += data;
+      console.log("Partial body: " + body);
+    });
+    req.on("end", async function () {
+      console.log("Body: " + body);
+      res.writeHead(200, { "Content-Type": "text/html" });
+      const html = await fetchUrl(JSON.parse(body)["url"]);
+      res.end(JSON.stringify(html));
+      // res.end("post received");
+    });
+  } else {
+    console.log("GET");
+  }
+});
+
+listener.listen(5050, "localhost", () => {
+  console.log("Server is listening on port 5050");
+});
+
+
+/*
+
 const express = require('express')
 const cors = require('cors');
 const axios = require('axios');
@@ -33,3 +77,4 @@ app.post('/',async (req,res)=>{
     }
        
 })
+*/
